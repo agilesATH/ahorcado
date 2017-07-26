a8a144525ca94566b3a75a3b299a467a9f2b58c7
kleer <kleer@kleer-csd-box>
2017-07-26 10:53:44 -0500

    2.Historia usuario - funcional

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require './config'
 
+palabra="AMOR"
+temporal=['_', '_', '_', '_']
+
 get '/' do	
 	session['acierto']="_ _ _ _"
 	erb(:index)
@@ -8,9 +11,18 @@ end
 
 
 post '/guardar'  do
-	session['letra'] = params['campo']
-	if session['letra'] == "A"
-		session['acierto']="A _ _ _"	
+	letratemp = params['campo']
+
+	(0..palabra.size).each do |index|
+		if palabra[index] == letratemp
+			temporal[index] = letratemp
+		end
 	end
+
+	#session['letra'] = letratemp
+	#posicion = palabra[letratemp]
+	#temporal[posicion]=letratemp
+	session['acierto']=temporal.join
+	#session['acierto'] = posicion
 	erb(:index)
 end
