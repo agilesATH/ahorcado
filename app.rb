ea9ac3ade8500e548b1eb5469e25a464027f5a08
kleer <kleer@kleer-csd-box>
2017-07-26 10:10:37 -0500

    US 2

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require './config'
 
+
+
 get '/' do
+	session['acierto']="_ _ _ _"
+	erb(:index)
+end
+
+post '/guardar' do 
+	#if session['letra']==""
+		
+	#else
+	#	session['acierto']="A _ _ _"
+	#end
 	erb(:index)
 end
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -6,3 +6,7 @@ Then(/^debo ver "([^"]*)"$/) do |texto|
   	expect(page.body).to match /#{texto}/m
 end
 
+Given(/^digito letra "([^"]*)"$/) do |letra|
+  expect(page.body).to match /#{letra}/m
+end
+
